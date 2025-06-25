e02c8c2dfdffb924b4838b00c143e91734070cd8
BrunoBorgess <lucasb.empreendimentos@gmail.com>
2025-06-25 12:51:28 -0300

    Att about entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topic/<int:topic_id>', views.topic, name='topic'),
+
+    
 
 ]
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from .models import Topic
 
 def index(request):
     """ Página principal Learning_Log """
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ Mostra todos os assuntos """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics }
+    return render(request, 'learning_logs/topics.html', context)
+
+
+
+
+def topic(request, topic_id):
+    """ Mostra um unico assunto e todas as suas entradas """
+    topic = Topic.objects.get(id = topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
