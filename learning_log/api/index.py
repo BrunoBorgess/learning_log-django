4af561b72f3d81ae7f521f72b5aab758e44d4106
BrunoBorgess <lucasb.empreendimentos@gmail.com>
2025-07-12 12:08:00 -0300

    Alteração funcionalidades vercel

---
+++ learning_log/api/index.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+# Adiciona a raiz do projeto ao sys.path
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+# Agora importa o WSGI
+from learning_log.wsgi import application
+
