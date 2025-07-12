ec9402b47dbd844c448100b056a64311d061a61c
BrunoBorgess <lucasb.empreendimentos@gmail.com>
2025-07-12 12:12:56 -0300

    Alterações Vercel deploy

--- learning_log/api/index.py
+++ learning_log/api/index.py
@@ -4,6 +4,6 @@ import os
 # Adiciona a raiz do projeto ao sys.path
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-# Agora importa o WSGI
+# Agora importa o WSGI da pasta correta
 from learning_log.wsgi import application
 
